a2af17883a9916b5eccab153f41081bb89f8ed83
AlperSocial <60712476+Alp-R@users.noreply.github.com>
2021-01-08 10:42:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log("hi");
+console.log("seems like you found the secret");
+console.log("https://www.youtube.com/watch?v=Nx-ZFAgIWzI");
